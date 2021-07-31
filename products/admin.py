a703382559a81fd92d66c9224edfe874ce35a8fe
shoreenb <shoreen.beckford@gmail.com>
2021-07-31 14:43:02 +0000

    Customise admin for categories, products and services

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+        )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- services/admin.py
+++ services/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Service, Category
 
-# Register your models here.
-admin.site.register(Service)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Category, CategoryAdmin)

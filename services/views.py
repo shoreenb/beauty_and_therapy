17a8ee2f90d0c721eb17b6aac73d80fef803887a
shoreenb <shoreen.beckford@gmail.com>
2021-07-31 16:52:00 +0000

    Add views and templates for products and services

--- services/views.py
+++ services/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Service
 
-# Create your views here.
+
+def all_services(request):
+    """
+    A view that shows all services as well as sorting and search queries
+    """
+
+    services = Service.objects.all()
+
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'services/services.html', context)

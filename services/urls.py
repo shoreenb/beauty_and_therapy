7bc2cf829b05c9c87831820bf302de4d1b6d604b
shoreenb <shoreen.beckford@gmail.com>
2021-08-01 16:27:39 +0000

    Add product_detail.html and service_detail.html

--- services/urls.py
+++ services/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_services, name='services')
+    path('', views.all_services, name='services'),
+    path('<service_id>', views.service_detail, name='service_detail'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Service
 
 
@@ -14,3 +14,17 @@ def all_services(request):
     }
 
     return render(request, 'services/services.html', context)
+
+
+def service_detail(request, service_id):
+    """
+    A view that shows the details of an individual service
+    """
+
+    service = get_object_or_404(Service, pk=service_id)
+
+    context = {
+        'service': service,
+    }
+
+    return render(request, 'services/service_detail.html', context)

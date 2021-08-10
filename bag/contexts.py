9e4d08c480e9d5a977f9214301795541f14d35a6
shoreenb <shoreen.beckford@gmail.com>
2021-08-10 09:54:37 +0000

    Update shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -7,34 +7,34 @@ from products.models import Product
 def bag_contents(request):
 
     bag_items = []
-    product_total = 0
-    product_count = 0
+    total = 0
+    count = 0
     bag = request.session.get('bag', {})
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        product_total += quantity * product.price
-        product_count += quantity
+        total += quantity * product.price
+        count += quantity
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
         })
 
-    if product_total < settings.FREE_GIFT_THRESHOLD:
-        gift = product_total * Decimal(
+    if total < settings.FREE_GIFT_THRESHOLD:
+        gift = total * Decimal(
                 settings.STANDARD_GIFT_PERCENTAGE / 100)
-        free_gift_delta = settings.FREE_GIFT_THRESHOLD - product_total
+        free_gift_delta = settings.FREE_GIFT_THRESHOLD - total
     else:
         gift = 0
         free_gift_delta = 0
 
-    grand_total = gift + product_total
+    grand_total = gift + total
 
     context = {
         'bag_items': bag_items,
-        'product_total': product_total,
-        'product_count': product_count,
+        'total': total,
+        'count': count,
         'gift': gift,
         'free_gift_delta': free_gift_delta,
         'free_gift_threshold': settings.FREE_GIFT_THRESHOLD,

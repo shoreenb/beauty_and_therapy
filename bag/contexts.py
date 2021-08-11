f40a836865e745553ba17c3f2680cba2313ae0a3
shoreenb <shoreen.beckford@gmail.com>
2021-08-11 11:23:52 +0000

    Complete product options logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,15 +11,27 @@ def bag_contents(request):
     count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for option, quantity in item_data['items_by_option'].items():
+                total += quantity * product.price
+                count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'option': option,
+                })
 
     if total < settings.FREE_GIFT_THRESHOLD:
         gift = total * Decimal(
--- bag/views.py
+++ bag/views.py
@@ -14,13 +14,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    print(redirect_url)
+    option = None
+    if 'product_option' in request.POST:
+        option = request.POST['product_option']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if option:
+        if item_id in list(bag.keys()):
+            if option in bag[item_id]['items_by_option'].keys():
+                bag[item_id]['items_by_option'][option] += quantity
+            else:
+                bag[item_id]['items_by_option'][option] = quantity
+        else:
+            bag[item_id] = {'items_by_option': {option: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)

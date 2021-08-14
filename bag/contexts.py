49e226990369faffa6b24d232f6eae70a380da7e
shoreenb <shoreen.beckford@gmail.com>
2021-08-14 01:20:51 +0000

    Add quantity box to shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -28,7 +28,7 @@ def bag_contents(request):
                 count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'option': option,
                 })
